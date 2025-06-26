c288e66a98ab5be8ca0e2e46ce9b3435ccd788bc
bridodev <contacto@brian-dominguez.dev>
2025-06-25 20:31:33 -0600

    FAB

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,27 @@ export default {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        shake: {
+          "0%, 100%": { transform: "translateX(0)" },
+          "25%": { transform: "translateX(-5px)" },
+          "75%": { transform: "translateX(5px)" },
+        },
+        gradient: {
+          "0%, 100%": {
+            backgroundPosition: "0% 50%",
+          },
+          "50%": {
+            backgroundPosition: "100% 50%",
+          },
+        },
+      },
+      animation: {
+        shake: "shake 0.5s ease-in-out",
+        gradient: "gradient 10s ease infinite",
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
